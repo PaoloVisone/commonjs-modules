7e3861ab44752647923a47f0eda06ee046233885
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-31 17:20:19 +0100

    [feat] add function (getName)

--- JS/names.js
+++ JS/names.js
@@ -1,4 +1,15 @@
 
 // Creo una funzione che restituisce nome e cognome
 
+function getName(firstName, lastName) {
+    const person = {
+        name: firstName,
+        surname: lastName
+    }
+    return person
+}
+
+console.log(getName("Paolo", "Visone"));
+
+
 
